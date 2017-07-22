49d41767b949360ce788731777d15b83bc2c4d2d
Vinícius Menezes <viny2999@gmail.com>
2017-07-22 12:18:48 -0300

    Implementação ADD Check Activity

--- app/src/main/java/br/com/damasceno/hackthon_cielo/AddCheck.java
+++ app/src/main/java/br/com/damasceno/hackthon_cielo/AddCheck.java
@@ -3,12 +3,11 @@ package br.com.damasceno.hackthon_cielo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AddCheck extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_add_check);
     }
 }
